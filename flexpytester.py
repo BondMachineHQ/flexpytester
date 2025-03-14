c0e21f61efb5fc59b70c52eac1cb90bdf1cfd304
Mirko Mariotti <mirko@mirkomariotti.it>
2025-03-14 17:17:57 +0100

    :construction: (flexpyteste) skeleton complete

--- flexpytester.py
+++ flexpytester.py
@@ -4,19 +4,25 @@
 """Flexpytester
 
 Usage:
-  flexpytester -e <expression> -o <outputfile> -i <inputfile> [-t <type>]
+  flexpytester --compute -e <expression> -o <outputfile> -i <inputfile> [-t <type>] [--csv] [--prefix]
+  flexpytester --generate -e <expression> -s <outputexpression> [-o <outputfile>] [-i <inputfile>] [-t <type>] [--csv] [--prefix]
   flexpytester -h | --help
 
 Options:
   -h --help                                         Show this screen.
-  -e <expression>                                   The expression to convert.
-  -o <outputfile>                                   The output file.
-  -i <inputfile>                                    The input file.
+  -c, --compute                                     Compute the expression ranging the inputs over the specified ranges.
+  -e <expression>                                   Input expression (when computing), symbols and ranges.
+  -o <outputfile>                                   The outputs file name for the generated outputs.
+  -i <inputfile>                                    The inputs file name for the generated inputs.
+  -s <outputexpression>                             The output expression (when generating).
   -t <type>                                         The type of the numbers, if not specified it is set to float32.
+  --csv                                             The output file is in CSV format.
+  --prefix                                          Prefix numbers with the prefix type as given by bmnumbers.
 """
 
 from docopt import docopt
 import sympy as sp
+import sys
 
 def main():
 	arguments = docopt(__doc__, version='Flexpytester 0.0')
@@ -29,18 +35,45 @@ def main():
 	expr = f.read()
 	f.close()
 
-	localParams = {'spExpr': None, 'testRanges': None}
-	globalParams = {'sp': sp}
-	exec(expr, globalParams, localParams)
-	spExpr = localParams['spExpr']
-	testRanges = localParams['testRanges']
+	if arguments["--compute"]:
+		localParams = {'spExpr': None, 'testRanges': None}
+		globalParams = {'sp': sp}
+		# exec(expr, globalParams, localParams)
+		spExpr = localParams['spExpr']
+		testRanges = localParams['testRanges']
 
-	if spExpr is None:
-		print("Error: The expression is not valid")
-		return
+		# TODO Generate the test ranges if all the parameters are valid
+		if testRanges != None and arguments["-o"] != None and arguments["-i"] != None:
+			print ("TODO")
 
-	# spExpr = sp.parse_expr(expr, evaluate=False)
-	# print(srepr(spEXpr))
+
+		if spExpr is None:
+			print("Error: The expression is not valid")
+			sys.exit(1)
+		
+	elif arguments["--generate"]:
+		localParams = {'symbols': None, 'testRanges': None}
+		globalParams = {'sp': sp}
+		# exec(expr, globalParams, localParams)
+		symbols = localParams['symbols']
+		testRanges = localParams['testRanges']
+
+		if symbols is None:
+			print("Error: Symbols not valid")
+			sys.exit(1)
+	
+		# TODO Place here the code to generate the expression
+
+		# TODO Generate also the test ranges if all the parameters are valid
+		if testRanges != None and arguments["-o"] != None and arguments["-i"] != None:
+			print ("TODO")
+
+	else:
+		print("Error: Invalid arguments")
+		sys.exit(1)
+	
+	print (arguments)
+
 
 if __name__ == '__main__':
 	main()
